99afba8d82fa14bbb701d3816d8293f046fa493c
EdoBergamo <hello@amtriix.it>
2023-12-01 05:42:38 +0000

    [UPDATE] Hide numbers before prompts appear

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ function getRandomNumber(min, max) {
 function generateRandomNumbers() {
   let numbers = [];
   while (numbers.length < 5) {
-    let randomNumber = getRandomNumber(1, 9);
+    let randomNumber = getRandomNumber(1, 100);
 
     if (!numbers.includes(randomNumber)) {
       numbers.push(randomNumber)
@@ -19,13 +19,21 @@ function displayNumbers(numbers) {
   numbersDiv.innerText = 'Numeri: ' + numbers.join(' ');
 }
 
-function getUserInput() {
-  let userNumbers = [];
-  for (let i = 0; i < 5; i++) {
-    let number = parseInt(prompt('Inserisci il numero ' + (i + 1)));
-    userNumbers.push(number);
-  }
-  return userNumbers;
+function getUserInput(callback) {
+  let numbersDiv = document.getElementById('numbers');
+  numbersDiv.style.display = 'none';
+
+  setTimeout(function() {
+    let userNumbers = [];
+    for (let i = 0; i < 5; i++) {
+      let number = parseInt(prompt('Inserisci il numero ' + (i + 1)));
+      userNumbers.push(number);
+    }
+    
+    numbersDiv.style.display = 'block';
+
+    callback(userNumbers);
+  }, 100);
 }
 
 function compareNumbers(randomNumbers, userNumbers) {
@@ -44,13 +52,13 @@ displayNumbers(randomNumbers);
 setTimeout(function() {
   document.getElementById('numbers').innerText = '';
 
-  let userNumbers = getUserInput();
+  getUserInput(function(userNumbers) {
+    let correctNumbers = compareNumbers(randomNumbers, userNumbers);
 
-  let correctNumbers = compareNumbers(randomNumbers, userNumbers);
-
-  if (correctNumbers.length > 0) {
-    alert('Hai indovinato ' + correctNumbers.length + ' numeri: ' + correctNumbers.join(', '));
-  } else {
-    alert('Nessun numero indovinato. Ritenta!');
-  }
+    if (correctNumbers.length > 0) {
+      alert('Hai indovinato ' + correctNumbers.length + ' numeri: ' + correctNumbers.join(', '));
+    } else {
+      alert('Nessun numero indovinato. Ritenta!');
+    }
+  });
 }, 30000);
